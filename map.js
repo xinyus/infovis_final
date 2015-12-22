a607e7929bf4ea972650274ec9668e857bc88d6e
xinyus <xinyush@umich.edu>
2015-12-22 11:41:57 -0500

    Merge branch 'weikaizh/develop' into develop
    # Conflicts:
    #	map.js

--- map.js
+++ map.js
@@ -12,7 +12,8 @@ d3.json("us.json", function(error, us) {
 
         var selectedYear = 2005;
         var selectedState = 60;
-        states = {"1":"Alabama", "2":"Alaska", "4":"Arizona", "5":"Arkansas", "6":"California", "8":"Colorado", "9":"Connecticut", "10":"Delaware", "11":"District of Columbia", "12":"Florida", "13":"Georgia", "15":"Hawaii", "16":"Idaho", "17":"Illinois", "18":"Indiana", "19":"Iowa", "20":"Kansas", "21":"Kentucky", "22":"Louisiana", "23":"Maine", "24":"Maryland", "25":"Massachusetts", "26":"Michigan", "27":"Minnesota", "28":"Mississippi", "29":"Missouri", "30":"Montana", "31":"Nebraska", "32":"Nevada", "33":"New Hampshire", "34":"New Jersey", "35":"New Mexico", "36":"New York", "37":"North Carolina", "38":"North Dakota", "39":"Ohio", "40":"Oklahoma", "41":"Oregon", "42":"Pennsylvania", "44":"Rhode Island", "45":"South Carolina", "46":"South Dakota", "47":"Tennessee", "48":"Texas", "49":"Utah", "50":"Vermont", "51":"Virginia", "53":"Washington", "54":"West Virginia", "55":"Wisconsin", "56":"Wyoming"};
+        var pollu_selected = 'co';
+        states = {"0": "U.S.", "1":"Alabama", "2":"Alaska", "4":"Arizona", "5":"Arkansas", "6":"California", "8":"Colorado", "9":"Connecticut", "10":"Delaware", "11":"District of Columbia", "12":"Florida", "13":"Georgia", "15":"Hawaii", "16":"Idaho", "17":"Illinois", "18":"Indiana", "19":"Iowa", "20":"Kansas", "21":"Kentucky", "22":"Louisiana", "23":"Maine", "24":"Maryland", "25":"Massachusetts", "26":"Michigan", "27":"Minnesota", "28":"Mississippi", "29":"Missouri", "30":"Montana", "31":"Nebraska", "32":"Nevada", "33":"New Hampshire", "34":"New Jersey", "35":"New Mexico", "36":"New York", "37":"North Carolina", "38":"North Dakota", "39":"Ohio", "40":"Oklahoma", "41":"Oregon", "42":"Pennsylvania", "44":"Rhode Island", "45":"South Carolina", "46":"South Dakota", "47":"Tennessee", "48":"Texas", "49":"Utah", "50":"Vermont", "51":"Virginia", "53":"Washington", "54":"West Virginia", "55":"Wisconsin", "56":"Wyoming"};
 
         function getStateName(stateCode){
         	return states[stateCode];
@@ -23,9 +24,15 @@ d3.json("us.json", function(error, us) {
             selectedState = stateCode;
 
             updateLineChart(parseInt(this.value));
+            if (selectedState === '0') {
+              selectedState = '60';
+            }
             heatUpdate(selectedYear, selectedState);
         });
         function updateLineChart(stateCode) {
+            if (stateCode === 60) {
+              stateCode = 0;
+            }
             $( "#states" ).val(stateCode);
             stateName = getStateName(stateCode);
             $("#headline_tag").find("h1").html(getStateName(stateCode));
@@ -180,7 +187,13 @@ d3.json("us.json", function(error, us) {
             // Update the SVG with the new data and call chart
         }
 
-        updateLineChart(selectedState - 60);
+        updateLineChart(selectedState);
+
+        $('input[name="pollutant"]:radio').on('click', function() {
+          console.log($(this).val());
+          pollu_selected = $(this).val();
+          update(selectedYear);
+        });
 
         var width = 960,
             height = 500,
@@ -189,7 +202,7 @@ d3.json("us.json", function(error, us) {
         var arrPollutant = ["CO", "NO\u2082", "O\u2083", "PM\u2081\u2080", "PM\u2082.\u2085", "SO\u2082"];
         var arrMonth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
-        var pollu_selected = 'so2';
+
 
         var projection = d3.geo.albersUsa()
             .scale(1070)
@@ -247,8 +260,6 @@ d3.json("us.json", function(error, us) {
           update(parseInt(this.value));
           // heatUpdate();
         });
-
-        maxNum = pollutant.yearMax[pollu_selected];
 
         function update(year) {
           colorMap = d3.scale.linear()
@@ -348,16 +359,16 @@ d3.json("us.json", function(error, us) {
         heatUpdate(2005,60);
 
         function clicked(d) {
-          console.log(d);
-
-          selectedState = d.id;
-          updateLineChart(selectedState);
-          heatUpdate(selectedYear, selectedState);
 
           if (d && centered !== d) {
             centered = d;
+            selectedState = d.id;
+            updateLineChart(selectedState);
+            heatUpdate(selectedYear, selectedState);
           } else {
             centered = null;
+            updateLineChart(60);
+            heatUpdate(selectedYear, 60);
           }
 
           g.selectAll("path")
